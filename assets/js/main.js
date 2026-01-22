ed67e47d2c0fd08866966013e9898c2e36725524
KoiKibugu <159245414+KoiKibugu@users.noreply.github.com>
2026-01-22 12:29:45 +0300

    Add role description for PSE in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1099,6 +1099,11 @@ const teamData = {
             role: "Production",
             description: "Ensures customers goods are produced at the highest quality possible with swiftness to assure continuous value from our products.",
             image: "../images/TEAM (2).JPG"
+        },
+        {
+            role: "PSE (Production Sync Engineer)",
+            description: "Ensures synchronization between production schedules and engineering standards for operational efficiency.",
+            image: "../images/TEAM (2).JPG"
         }
     ]
 };
